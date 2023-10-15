f66747660e8e6146101d1dd621eabd7673362d74
Marcelle Fontoura <marcelle_sfs@hotmail.com>
2023-10-15 02:01:14 -0300

    commit inicial

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"AI Replace the Human Workforce? Myths and Facts about AI",
+    "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio perspiciatis, quasi asperiores accusamus iusto laudantium unde quod! Neque veritatis sed deleniti ipsa, odit ipsam modi sapiente pariatur dignissimos earum.",
+    "photoCover":"https://www.eweek.com/wp-content/uploads/2023/09/ew_09272023-will-ai-replace-jobs.png"
+  },
+
+  {
+    "id":"2",
+    "title":"Types of Artificial Intelligence",
+    "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui distinctio perspiciatis, quasi asperiores accusamus iusto laudantium unde quod! Neque veritatis sed deleniti ipsa, odit ipsam modi sapiente pariatur dignissimos earum.",
+    "photoCover":"https://www.fingent.com/wp-content/uploads/Untitled-1-02-1024x555-1.png"
+  },
+]
